fe1ca3a69de561c1482154ab18e71e6580ef4e69
Keshav Singh <kesin@microsoft.com>
2024-09-25 02:09:40 +0000

    Updated DatabricksUnityCatalog.py

--- pdgdqsourceanalyzer/src/DatabricksUnityCatalog.py
+++ pdgdqsourceanalyzer/src/DatabricksUnityCatalog.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-import pyodbc
+from databricks import sql
 
 class DatabricksUnityCatalogRequest(BaseModel):
     hostname: str
@@ -7,43 +7,26 @@ class DatabricksUnityCatalogRequest(BaseModel):
     access_token: str
 
     def test_connection(hostname, http_path, access_token):
-        port = 443  # Default port for Databricks ODBC
-
-        # Create a connection string
-        connection_string = (
-            f"DRIVER={{Simba Spark ODBC Driver}};"
-            f"HOST={hostname};"
-            f"PORT={port};"
-            f"HTTPPath={http_path};"
-            f"AuthMech=3;"  # AuthMech=3 indicates token-based authentication
-            f"UID=token;"
-            f"PWD={access_token};"
-            f"SparkServerType=3;"  # Use 3 for Databricks clusters
-            f"SSL=1;"  # Enable SSL
-            f"ThriftTransport=2;"  # Use HTTP transport mode
-            f"SparkSQLCatalogImplementation=hive;"
-        )
-
         try:
-            # Establish a connection
-            connection = pyodbc.connect(connection_string, autocommit=True)
+            conn = sql.connect(
+                server_hostname=hostname,
+                http_path=http_path,
+                access_token=access_token,
+            )
+            cursor = conn.cursor()
+            cursor.execute(f"SHOW CATALOGS")
 
-            # Create a cursor and execute a test query
-            cursor = connection.cursor()
-            cursor.execute("SHOW CATALOGS")
-
-            # Fetch results
             catalogs = cursor.fetchall()
             catalog_names = [catalog[0] for catalog in catalogs]
 
-            # Close the cursor and connection
             cursor.close()
-            connection.close()
+            conn.close()
 
             return {"status": "success", "catalogs": catalog_names}
 
         except Exception as e:
             return {"status": "error", "message": str(e)}
+
 
 class DatabricksUnityCatalogSchemaRequest(BaseModel):
     hostname: str
@@ -54,39 +37,23 @@ class DatabricksUnityCatalogSchemaRequest(BaseModel):
     table: str
 
     def get_table_schema(hostname, http_path, access_token, catalog, unitycatalogschema, table):
-        port = 443  # Default port for Databricks ODBC
-
-        # Create a connection string
-        connection_string = (
-            f"DRIVER={{Simba Spark ODBC Driver}};"
-            f"HOST={hostname};"
-            f"PORT={port};"
-            f"HTTPPath={http_path};"
-            f"AuthMech=3;"
-            f"UID=token;"
-            f"PWD={access_token};"
-            f"SparkServerType=3;"
-            f"SSL=1;"
-            f"ThriftTransport=2;"
-            f"SparkSQLCatalogImplementation=hive;"
-        )
-
         try:
-            # Establish a connection
-            connection = pyodbc.connect(connection_string, autocommit=True)
+            conn = sql.connect(
+                server_hostname=hostname,
+                http_path=http_path,
+                access_token=access_token,
+            )
 
-            # Create a cursor and execute the query to get the table schema
-            cursor = connection.cursor()
-            query = f"DESCRIBE {catalog}.{unitycatalogschema}.{table}"
+            cursor = conn.cursor()
+            query = f"DESCRIBE TABLE {catalog}.{unitycatalogschema}.{table}"
             cursor.execute(query)
 
             # Fetch the schema (column name and data type)
             columns = cursor.fetchall()
             schema_info = [{"column_name": row[0], "data_type": row[1]} for row in columns]
 
-            # Close the cursor and connection
             cursor.close()
-            connection.close()
+            conn.close()
 
             return {"status": "success", "schema": schema_info}
 
